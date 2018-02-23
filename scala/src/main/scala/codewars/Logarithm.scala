46c305e13d56d04c13dc55f0e909742cb2d8d1aa
jeronimo.garcia <jeronimo.garcia-loygorri@bbva.com>
2018-02-23 14:11:58 +0100

    feat: Finished complex logarithm

--- scala/src/main/scala/codewars/Logarithm.scala
+++ scala/src/main/scala/codewars/Logarithm.scala
@@ -4,9 +4,12 @@ object ComplexAlgorithm extends App{
 
   def log(complex: Array[Double]): Array[Double] = {
     if (complex(0) != 0 || complex(1) != 0){
-      return Array[Double](0,0)
+      val x = complex(0)
+      val y = complex(1)
+      return Array[Double](Math.log(Math.sqrt(x*x + y*y)), Math.atan2(y,x))
+
     } else{
-      throw new ArithmeticException("") // do it!
+      throw new ArithmeticException("")
     }
   }
 }
--- scala/src/test/scala/codewars/ComplexAlgSpec.scala
+++ scala/src/test/scala/codewars/ComplexAlgSpec.scala
@@ -7,10 +7,18 @@ import codewars.CustomCodewarsMatchers._
 class ComplexAlgSpec extends FunSpec with Matchers {
   describe("The complex algorthim function"){
 
-    it("Should work for normal numbers"){
+    it("should pass de example tests"){
       ComplexAlgorithm.log(Array[Double](20, 0)) should complexEqual (Array[Double](2.995732273553991, 0))
       ComplexAlgorithm.log(Array[Double](-1, 0)) should complexEqual (Array[Double](0, 3.141592653589793))
       ComplexAlgorithm.log(Array[Double](1, 1)) should complexEqual (Array[Double](0.346573590279973, 0.785398163397448))
+    }
+
+    it("should pass the fixed tests"){
+      ComplexAlgorithm.log(Array[Double](-24, 7)) should complexEqual (Array[Double](3.218875824868201, 2.857798544381465))
+      ComplexAlgorithm.log(Array[Double](0, 1)) should complexEqual (Array[Double](0, 1.570796326794897))
+      ComplexAlgorithm.log(Array[Double](1, 0)) should complexEqual (Array[Double](0, 0))
+      ComplexAlgorithm.log(Array[Double](8834d / 1997d, 93387d / 42243d)) should complexEqual (Array[Double](1.598433436873693, 0.463447163733142))
+      ComplexAlgorithm.log(Array[Double](28347, 73623)) should complexEqual (Array[Double](11.275831160977204, 1.203262110987354))
     }
 
     it("Should throw an exception if called on the poles"){
@@ -18,6 +26,5 @@ class ComplexAlgSpec extends FunSpec with Matchers {
         ComplexAlgorithm.log(Array[Double](0, 0))
       }
     }
-
   }
 }

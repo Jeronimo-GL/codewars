39375a3a5821ff8544838253947fc04376f569db
Jerónimo García-Loygorri <loygorri@gmail.com>
2018-03-15 00:05:43 +0100

    feat: Done cartesianDistance

--- go/codewars/cartesianDistance/cartesianDistance.go
+++ go/codewars/cartesianDistance/cartesianDistance.go
@@ -1,7 +1,6 @@
 package cartesianDistance
 
 import "math"
-import "fmt"
 
 
 func CartesianNeighborsDistance(x, y, r int) ([]float64){
@@ -9,10 +8,21 @@ func CartesianNeighborsDistance(x, y, r int) ([]float64){
 	
 	for i:=1; i<=r ; i++{
 		for j:=0; j<=i ; j++ {
-			fmt.Printf("%f\n", math.Sqrt(float64(i*i) + float64(j*j)))
-			resp=append(resp, math.Sqrt(float64(i*i) + float64(j*j)))
+			d:=math.Sqrt(float64(i*i) + float64(j*j))
+			if !arrayContains(resp, d) {
+				resp=append(resp, d)
+			}
 		}
 	}
 
   return resp
+}
+
+func arrayContains(list []float64, v float64) bool {
+	for _, n := range list {
+		if v==n {
+			return true
+		}
+	}
+	return false
 }

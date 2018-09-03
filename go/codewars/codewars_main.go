874b53700d87a131edc7ed17e3e344749d71c61a
Jerónimo García-Loygorri <loygorri@gmail.com>
2018-09-03 20:56:21 +0200

    feat: Works with arbitrary Actions but fails the tests

--- go/codewars/codewars_main.go
+++ go/codewars/codewars_main.go
@@ -11,21 +11,13 @@ import (
 func main() {
 	m := NewMachine()
 
-	_actions := []func(int) int{
-		func(x int) int { return x + 1 },
-		func(x int) int { return 0 },
-		func(x int) int { return int(x / 2) },
-		func(x int) int { return x * 100 },
-		func(x int) int { return x % 2 }}
-
-
 	for i:=0; i<100; i++{
-		COMMAND:=rand.Intn(5)
+		COMMAND:=rand.Intn(len(Actions()))
 		res:=m.Command(COMMAND, i)
 //		fmt.Printf("val:%d\t res:%d\texpected:%d\n", i, res, _actions[COMMAND](i))
-		m.Response(res==_actions[COMMAND](i))
+		m.Response(res==Actions()[COMMAND](i))
 		//		m.PrintState()
-		if res==_actions[COMMAND](i) {
+		if res==Actions()[COMMAND](i) {
 			fmt.Print("+")
 		}else{
 			fmt.Print("-")

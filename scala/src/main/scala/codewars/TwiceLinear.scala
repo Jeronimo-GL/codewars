ff7102bc37f79201be9c8172a8246fe94fdbd439
jeronimo.garcia <jeronimo.garcia-loygorri@bbva.com>
2018-11-29 16:49:51 +0100

    feat: TwiceLinear en scala

--- scala/src/main/scala/codewars/TwiceLinear.scala
+++ scala/src/main/scala/codewars/TwiceLinear.scala
@@ -4,51 +4,60 @@ package codewars
 
 object DblLinear extends App{
 
-  println(time {DblLinear.dblLinear(10)})
-//  println(time {DblLinear.dblLinear(1000)})
-//  println(time {DblLinear.dblLinear(10000)})
+  println(time {DblLinear.dblLinear(100)}) 
+  println(time {DblLinear.dblLinear(1000)})
+  println(time {DblLinear.dblLinear(10000)})
 
   def time[R](block: => R): R = {
     val t0 = System.nanoTime()
     val result = block    // call-by-name
     val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0)/1000 + " ms")
+    print(f" Elapsed time: ${(t1 - t0)/1000000.0}%f ms ")
     result
   }
 
+  def isIn(num: Int, comps:Array[Int], max: Int): Boolean = {
+    var i=0
+    while (i<=max){
+      if (comps(i)==num) return true
+      i+=1
+    }
+    return false
+  }
+
   def dblLinear(n: Int): Int = {
-    // your code
-    val twos = new Array[Int](n)
-    val threes = new Array[Int](n)
+    // your code 
+    val twos = new Array[Int](n+1)
+    val threes = new Array[Int](n+1)
     twos(0)=3
     threes(0)=4
 
     var next = 1
     var lastTwo = 0
     var lastThree = 0
+    var nextTwo = 0
+    var nextThree = 0
 
-    for (i <- 2 to n+1){
-      if (twos(lastTwo) <= threes(lastThree)){
-        next=twos(lastTwo)
-        lastTwo += 1
+    for (i <- 1 to n){
+      if (twos(nextTwo) <= threes(nextThree)){
+        next=twos(nextTwo)
+        nextTwo += 1
       } else {
-        next= threes(lastThree)
-        lastThree +=1
+        next= threes(nextThree)
+        nextThree += 1
       }
-
-      if (!threes.contains(next*2+1)){
+//      if (!threes.slice(0, lastThree).contains(next*2+1)){
+      if (!isIn(next*2+1, threes, lastThree)){  
+        lastTwo +=1
         twos(lastTwo) =(next*2 +1)
-        println("[2] "+ lastTwo + " " + twos(lastTwo))
       }
-
-      if (!twos.contains(next*3+1)){
+//      if (!twos.slice(0, lastTwo).contains(next*3+1)){
+      if (!isIn(next*3+1, twos, lastTwo)){  
+        lastThree += 1
         threes(lastThree)= (next*3 +1)
-        println("[3] "+ lastThree + " " + threes(lastThree))
+
       }
     }
-    // for (i <- 0 to n-1){
-    //   println(twos(i) + " " + threes(i))
-    // }
     return next
   }
 }

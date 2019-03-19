b88013b3d9faacdf03f54a45fb9c8fa969692ce6
jeronimo.garcia <jeronimo.garcia-loygorri@bbva.com>
2019-03-19 15:33:14 +0100

    Anagrams in Python

---
+++ python/anagrams/anagrams.py
@@ -0,0 +1,14 @@
+def anagrams(word, words):
+    wl=sorted(list(word))
+    retList=[]
+    print("wl", wl)
+    for w in words:
+        ll= sorted(list(w))
+        print("ll", ll)
+        if ll == wl :
+            retList.append(w)
+    return retList
+
+
+if __name__ == '__main__':
+    print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
